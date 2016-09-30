020188dc475e80e84c6c6924dd44e2fa55ee948a
Mads Skovgaard <madsskovgaard@hotmail.com>
2016-09-30 16:03:28 +0200

    Fixes issue with block sides rotating.

--- src/main/java/fair/tfcengineer/common/Render/SidedBlockRender.java
+++ src/main/java/fair/tfcengineer/common/Render/SidedBlockRender.java
@@ -4,6 +4,7 @@ import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.src.FMLRenderAccessLibrary;
 import net.minecraft.world.IBlockAccess;
 import org.lwjgl.opengl.GL11;
 
@@ -14,11 +15,8 @@ public class SidedBlockRender implements ISimpleBlockRenderingHandler {
         float min = 1f / 16f;
         float max = 15f / 16f;
 
-        metadata = 5; // Meta should be the side I want to be front, so east.
-
         renderer.setRenderBounds(min, min, min, max, max, max);
         renderInvBlock(block, metadata, renderer);
-        renderer.clearOverrideBlockTexture();
     }
 
     @Override
@@ -28,31 +26,33 @@ public class SidedBlockRender implements ISimpleBlockRenderingHandler {
     }
 
     public static void renderInvBlock(Block block, int m, RenderBlocks renderer) {
+        // This is all pretty much taken from renderer.renderBlockAsItem()
         Tessellator tes = Tessellator.instance;
+        GL11.glRotatef(-90.0F, 0.0F, 1.0F, 0.0F); // Makes sure it is the front side that is rendered
         GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
         tes.startDrawingQuads();
         tes.setNormal(0.0F, -1.0F, 0.0F);
-        renderer.renderFaceYNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(0, m)); // bottom
+        renderer.renderFaceYNeg(block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(block, 0, m));
         tes.draw();
         tes.startDrawingQuads();
         tes.setNormal(0.0F, 1.0F, 0.0F);
-        renderer.renderFaceYPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(1, m)); // top
-        tes.draw();
-        tes.startDrawingQuads();
-        tes.setNormal(-1.0F, 0.0F, 0.0F);
-        renderer.renderFaceXNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(3, m)); // south
+        renderer.renderFaceYPos(block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(block, 1, m));
         tes.draw();
         tes.startDrawingQuads();
         tes.setNormal(0.0F, 0.0F, -1.0F);
-        renderer.renderFaceZNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(4, m)); // west
-        tes.draw();
-        tes.startDrawingQuads();
-        tes.setNormal(-1.0F, 0.0F, 0.0F);
-        renderer.renderFaceXPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(5, m)); // east
+        renderer.renderFaceZNeg(block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(block, 2, m));
         tes.draw();
         tes.startDrawingQuads();
         tes.setNormal(0.0F, 0.0F, 1.0F);
-        renderer.renderFaceZPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(2, m)); // north
+        renderer.renderFaceZPos(block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(block, 3, m));
+        tes.draw();
+        tes.startDrawingQuads();
+        tes.setNormal(-1.0F, 0.0F, 0.0F);
+        renderer.renderFaceXNeg(block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(block, 4, m));
+        tes.draw();
+        tes.startDrawingQuads();
+        tes.setNormal(1.0F, 0.0F, 0.0F);
+        renderer.renderFaceXPos(block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(block, 5, m));
         tes.draw();
         GL11.glTranslatef(0.5F, 0.5F, 0.5F);
     }
